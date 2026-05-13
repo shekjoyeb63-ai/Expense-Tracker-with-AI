7193ae7d30ee6ecec4f1ae6d93c7c924f31109de
Sheak Md Joyeb <sheikhjoyeb849@gmail.com>
2026-05-13 13:44:52 +0530

    updating few logics

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-celery -A celery_app worker --loglevel=info --pool=solo &
+PYTHONPATH=/app celery -A celery_app worker --loglevel=info --pool=solo &
 gunicorn run:app --bind 0.0.0.0:$PORT --workers 2
